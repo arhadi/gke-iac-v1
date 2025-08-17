5a50af0213064e539c8fe6662ca2f8de19896e0f
Arif Rustam Hadi <arif.rustam@gmail.com>
2025-08-17 21:43:27 +0700

    v3

--- variables.tf
+++ variables.tf
@@ -5,4 +5,7 @@ variable "credentials_json" { type = string sensitive = true }
 variable "cluster_name" { type = string default = "gke-prod-autopilot-01" }
 variable "primary_cidr" { type = string default = "10.10.10.0/24" }
 variable "pods_cidr" { type = string default = "10.10.100.0/20" }
-variable "services_cidr" { type = string default = "10.10.200.0/24" }
+variable "services_cidr" { 
+    type = string 
+    default = "10.10.200.0/24" 
+    }
